9e53f010d31530b63b4b235908aa4b8cfd3b0f59
Mario Kuraoka <kuraoka@gmail.com>
2025-11-09 20:25:50 -0300

    v1.0.3

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // Health check endpoint for Kubernetes probes
 app.get('/', (req, res) => {
-  res.json({ version: '1.0.2' });
+  res.json({ version: '1.0.3' });
 });
 
 // Health check endpoint for Kubernetes probes
